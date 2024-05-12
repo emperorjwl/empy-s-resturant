10e25873847feb57d4aad34df4a80c474828aa9a
emperorjwl <jeweladegor21@gmail.com>
2024-05-12 07:13:08 +0100

    moaney app

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,28 @@
-import Header from "./ui/component/header";
-import Slides from "./ui/component/slides";
-import Story from "./ui/component/story";
-import Chefs from "./ui/component/chefs";
-import Menu from "./ui/component/menu";
+"use client";
+
+import { useState } from "react";
 
 export default function Page() {
+  var [num, update] = useState(0);
+
+  function handleButtonClick() {
+    num += 1000000;
+    update(num);
+  }
+
+  function handleButtonClick2() {
+    update((prevNum) => Math.max(prevNum - 1000000, 0));
+  }
+
   return (
     <main>
-      <Header />
-      <Slides />
-      <Story />
-      <Chefs />
-      <Menu />
+      <h1>My Money Page</h1>
+      <p>My Savings for the Year</p>
+      <p className="money">NGN {num}</p>
+      <button onClick={handleButtonClick}>+</button>
+      <button className="button2" onClick={handleButtonClick2}>
+        +
+      </button>
     </main>
   );
 }
