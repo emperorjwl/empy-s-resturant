ed054f83d22672d17ca6115a8cce71a480b40582
emperorjwl <jeweladegor21@gmail.com>
2024-05-18 18:33:00 +0100

    calculator

--- app/page.jsx
+++ app/page.jsx
@@ -2,28 +2,63 @@
 
 import { useState } from "react";
 
-export default function Page() {
-  var [num, update] = useState(0);
-
-  function handleButtonClick() {
-    num += 1000000;
-    update(num);
+export default function Home() {
+  var [screen, updateScreen] = useState("");
+  function handleButtonCLick(evt) {
+    screen += evt.target.innerHTML;
+    updateScreen(screen);
   }
 
-  function handleButtonClick2() {
-    num -= 1000000;
-    update(num);
+  function handleEvaluation() {
+    screen = eval(screen);
+    updateScreen(screen);
+  }
+
+  function handleClear() {
+    screen = "";
+    updateScreen(screen);
   }
 
   return (
     <main>
-      <h1>My Money Page</h1>
-      <p>My Savings for the Year</p>
-      <p className="money">NGN {num}</p>
-      <button onClick={handleButtonClick}>+</button>
-      <button className="button2" onClick={handleButtonClick2}>
-        -
-      </button>
+      <div className="calcname">
+        <h2>Empy's Calculator</h2>
+      </div>
+
+      <div className="res"> 
+        <label>{screen}</label>
+      </div>
+
+      <div className="buttons">
+        <button onClick={handleButtonCLick}>1</button>
+        <button onClick={handleButtonCLick}>2</button>
+        <button onClick={handleButtonCLick}>3</button>
+        <button onClick={handleButtonCLick}>4</button>
+        <button onClick={handleButtonCLick}>5</button>
+        <button onClick={handleButtonCLick}>6</button>
+        <button onClick={handleButtonCLick}>7</button>
+        <button onClick={handleButtonCLick}>8</button>
+        <button onClick={handleButtonCLick}>9</button>
+        <button onClick={handleButtonCLick}>0</button>
+        <button onClick={handleButtonCLick} className="operator">
+          +
+        </button>
+        <button onClick={handleButtonCLick} className="operator">
+          -
+        </button>
+        <button onClick={handleButtonCLick} className="operator">
+          /
+        </button>
+        <button onClick={handleButtonCLick} className="operator">
+          *
+        </button>
+        <button onClick={handleEvaluation} className="equal">
+          =
+        </button>
+        <button onClick={handleClear} className="remove">
+          CLEAR
+        </button>
+      </div>
     </main>
   );
 }
